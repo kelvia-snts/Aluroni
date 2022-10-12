a6f8fb2182661adfa580a666e82d7b5f11aec7db
Kelvia Santos <kelviaks@gmail.com>
2022-10-12 11:05:55 -0300

    feat:add fonts and normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "normalize.css";
 import "./index.css";
 import RestaurantMenu from "./pages/RestaurantMenu";
 
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RestaurantMenu/>
+    <RestaurantMenu />
   </React.StrictMode>
 );
