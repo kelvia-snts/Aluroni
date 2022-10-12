ad3f06b425357e1195d99728f9dfb38ac0fee927
Kelvia Santos <kelviaks@gmail.com>
2022-10-12 09:51:07 -0300

    feat:create page structure and install sass

---
+++ src/pages/RestaurantMenu/index.tsx
@@ -0,0 +1,9 @@
+import restaurantMenuStyle from "./RestaurantMenu.module.sass";
+
+export default function RestaurantMenu() {
+  return (
+    <main>
+      <nav className="menu"></nav>
+    </main>
+  );
+}
