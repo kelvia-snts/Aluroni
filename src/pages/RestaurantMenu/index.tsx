9ff0655b4912972874d102a37ca7c4bc51fac631
Kelvia Santos <kelviaks@gmail.com>
2022-10-12 10:45:29 -0300

    feat:add logo, change img tag to an svg component, reset css, add absolute imports.

--- src/pages/RestaurantMenu/index.tsx
+++ src/pages/RestaurantMenu/index.tsx
@@ -1,10 +1,13 @@
 import Button from "../../Components/Button";
 import restaurantMenuStyle from "./RestaurantMenu.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
 export default function RestaurantMenu() {
   return (
     <main>
-      <nav className="menu"></nav>
+      <nav className={restaurantMenuStyle.menu}>
+        <Logo />
+      </nav>
       <Button />
     </main>
   );
